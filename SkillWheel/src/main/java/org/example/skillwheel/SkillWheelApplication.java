45bd25ac5cc799d4fe11ba0335ff9ad25ba6d972
Iwo Sidorowicz <sidorowi@NY9RF279GN.ad.here.com>
2025-03-04 02:14:07 +0100

    Added initial code

---
+++ SkillWheel/src/main/java/org/example/skillwheel/SkillWheelApplication.java
@@ -0,0 +1,13 @@
+package org.example.skillwheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillWheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillWheelApplication.class, args);
+	}
+
+}
---
+++ SkillWheel/src/test/java/org/example/skillwheel/SkillWheelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skillwheel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillWheelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
